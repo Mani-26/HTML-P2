125ff8f05c6483ddf89223f91892b6acfa6c8fab
MANI S <maniinnovator26@gmail.com>
2023-07-13 14:38:04 +0530

    anime

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,18 @@
+function myf() {
+    document.getElementById('demo').innerHTML = "paragraph that was changed";
+    document.getElementById('demo').style.fontSize = '34px';
+    document.getElementById('demo').style.color = 'green';
+    alert('Do you Want to Continue!');
+}
+function mydivi() {
+    while (true) {
+        console.log(Math.random(0,1000)+Math.random(0,10));
+        
+        alert('Do you Want to Continue!');
+    }
+}
+console.log(5+55);
+while (true) {
+    console.log(Math.random(0,1000)+Math.random(0,10));
+    
+}
